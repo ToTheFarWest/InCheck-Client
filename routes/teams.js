e54cb5a7538757d78da78c45da9f2a132486370e
Yarden Akin <yardenakin@gmail.com>
2020-05-15 01:15:35 +0300

    Implemented team list

--- routes/teams.js
+++ routes/teams.js
@@ -1,17 +1,39 @@
 const express = require('express');
 const router = express.Router();
 let service = require('../service');
-const auth = require("../middeware/authorization");
+const auth = require("../middleware/authorization");
 
-router.get('/', auth, (req, res) => {
-    res.render('teams', {
-        title: "Teams",
-        token: req.token,
-        teams: [
-            {name: "the boys", id: "itstheboysid"},
-            {name: "are back", id: "webackid"}
-        ]
-    });
+router.get('/', auth, async (req, res) => {
+    try {
+        const path = '/teams';
+        const headers = {
+            "Authorization": `Bearer ${req.token}`
+        };
+        const result = await service.get(path, {headers: headers});
+        // console.log(result);
+        const teams = result.data.teams;
+
+        res.render('teams', {
+            title: "Teams",
+            token: req.token,
+            teams: teams
+        });
+    }
+    catch (err) {
+        console.log(err);
+        res.send("ERROR SAD");
+    }
+
+});
+
+router.get('/:id', async (req, res) => {
+    try{
+        res.send(req.params.id);
+    }
+    catch (err) {
+        console.log(err);
+        res.send("ERROR");
+    }
 });
 
 module.exports = router;
